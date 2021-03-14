253c675f078586327267a3ca45ee2faefd36aa29
Goten <64665142+wavygoten@users.noreply.github.com>
2021-03-14 13:52:43 -0500

    added walmart sku finder + bug fixes

--- bot.js
+++ bot.js
@@ -1,99 +1,113 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
-const token = {BOT-TOKEN-HERE};
+const tokens = [
+  {BOT-TOKEN},
+  {BOT-TOKEN},
+];
 const prefix = "-";
 const axios = require("axios");
 const cheerio = require("cheerio");
+const fs = require("fs");
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+for (const token of tokens) {
+  const client = new Discord.Client();
 
-client.on("message", (message) => {
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  if (command == "variants") {
+  client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
+
+  client.on("message", (message) => {
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
     if (!message.content.startsWith(prefix) || message.author.bot) return;
-    const url = args[0];
-    let lsize = "";
-    let lvar = "";
-    let data = [];
-    try {
-      axios({
-        method: "get",
-        url: url,
-      }).then(function (response) {
-        document = response.data;
-        const $ = cheerio.load(document);
-        var title = $('meta[property="og:title"]').attr("content");
-        var img = $('meta[property="og:image"]').attr("content");
-        var titletext = $("title").text();
-        var price;
+    if (command == "variants") {
+      const url = args[0];
+      let lsize = "";
+      let lvar = "";
+      let data = [];
+      try {
+        axios({
+          method: "get",
+          url: url,
+        })
+          .then(function (response) {
+            document = response.data;
+            const $ = cheerio.load(document);
+            var title = $('meta[property="og:title"]').attr("content");
+            var img = $('meta[property="og:image"]').attr("content");
+            var titletext = $("title").text();
+            var price;
 
-        if (!img.match(/^[a-zA-Z]+:\/\//)) {
-          img = "https:" + img;
-        } else {
-          img = img;
-        }
-        if (title === undefined) title = titletext;
-
-        const getAfterText = (allText, keyword) => {
-          return allText.slice(allText.indexOf(keyword));
-        };
-        parsedData = JSON.parse(
-          getAfterText(document, "var meta = ").split(";", 1)[0].substring(11)
-        );
-        for (size in parsedData.product.variants) {
-          lsize += "\n" + parsedData.product.variants[size].public_title;
-        }
-        for (variant in parsedData.product.variants) {
-          lvar += "\n" + parsedData.product.variants[variant].id;
-        }
-
-        price = parsedData.product.variants[0].price / 100;
-        
-        const embed = new Discord.MessageEmbed()
-          .setColor("#f09719")
-          .setTitle(title)
-          .setURL(url)
-          .addFields(
-            {
-              name: "Sizes",
-              value: "```" + lsize + "```",
-              inline: true,
-            },
-            {
-              name: "Variants",
-              value: "```" + lvar + "```",
-              inline: true,
-            },
-            {
-              name: "Price",
-              value: "$" + price + ".00",
-              inline: false,
+            if (img == undefined) {
+              img = "";
+            } else if (!img.match(/^[a-zA-Z]+:\/\//)) {
+              img = "https:" + img;
+            } else {
+              img = img;
             }
-          )
-          .setThumbnail(img)
-          .setFooter(
-            message.guild.name + " - " + message.author.username,
-            message.guild.iconURL()
-          );
-        message.channel.send(embed);
-      }).catch(() => (error) => {
-          const embederror = new Discord.MessageEmbed()
-            .setColor("#f09719")
-            .setDescription("Error Retrieving Variants");
-          message.send.channel(embederror);
-          console.log(error);
-        });
-    } catch (error) {
-      const embed = new Discord.MessageEmbed()
-        .setColor("#f09719")
-        .setDescription("Error Retrieving Variants");
-      console.log(error);
+            if (title === undefined) title = titletext;
+
+            const getAfterText = (allText, keyword) => {
+              return allText.slice(allText.indexOf(keyword));
+            };
+            parsedData = JSON.parse(
+              getAfterText(document, "var meta = ")
+                .split(";", 1)[0]
+                .substring(11)
+            );
+            for (size in parsedData.product.variants) {
+              lsize += "\n" + parsedData.product.variants[size].public_title;
+            }
+            for (variant in parsedData.product.variants) {
+              lvar += "\n" + parsedData.product.variants[variant].id;
+            }
+
+            price = (parsedData.product.variants[0].price / 100).toFixed(2);
+            // console.log(document.getElementsByTagName("*"));
+
+            const embed = new Discord.MessageEmbed()
+              .setColor("#f09719")
+              .setTitle(title)
+              .setURL(url)
+              .addFields(
+                {
+                  name: "Sizes",
+                  value: "```" + lsize + "```",
+                  inline: true,
+                },
+                {
+                  name: "Variants",
+                  value: "```" + lvar + "```",
+                  inline: true,
+                },
+                {
+                  name: "Price",
+                  value: "$" + price,
+                  inline: false,
+                }
+              )
+              .setThumbnail(img)
+              .setFooter(
+                message.guild.name + " - " + message.author.username,
+                message.guild.iconURL()
+              );
+            message.channel.send(embed);
+          })
+          .catch(() => (error) => {
+            const embederror = new Discord.MessageEmbed()
+              .setColor("#f09719")
+              .setDescription("Error Retrieving Variants");
+            message.send.channel(embederror);
+            console.log(error);
+          });
+      } catch (error) {
+        const embederror = new Discord.MessageEmbed()
+          .setColor("#f09719")
+          .setDescription("Error Retrieving Variants");
+        message.send.channel(embederror);
+        console.log(error);
+      }
     }
-  }
-  if (command == "stockx") {
+    if (command == "stockx") {
       const query = args;
       try {
         axios({
@@ -187,7 +201,79 @@ client.on("message", (message) => {
         console.log(error);
       }
     }
-});
+    if (command == "walmart") {
+      const uri = args[0];
+      try {
+        axios({
+          method: "get",
+          url: `${uri}`,
+          headers: {
+            "User-Agent":
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+            Accept: "*/*",
+            "Accept-Encoding": "gzip, deflate, br",
+            Connection: "keep-alive",
+          },
+        })
+          .then(function (response) {
+            // fs.writeFile("text.html", document, (err) => {
+            //   // In case of a error throw err.
+            //   if (err) throw err;
+            // });
+            let document = response.data;
 
-client.login(token);
+            const $ = cheerio.load(document);
+            let offerID = $('script[id="item"]')[0].children[0].data;
+            let parsedData = JSON.parse(offerID);
+            let offerId = parsedData.item.product.buyBox.products[0].offerId;
+            let title = parsedData.item.product.buyBox.products[0].productName;
+            let price =
+              parsedData.item.product.buyBox.products[0].priceMap.price;
+            let img = parsedData.item.product.buyBox.products[0].images[0].url;
 
+            const embed = new Discord.MessageEmbed()
+              .setColor("#f09719")
+              .setTitle(title)
+              .setURL(uri)
+              .addFields(
+                {
+                  name: "ID SKU",
+                  value: "```" + offerId + "```",
+                  inline: true,
+                },
+                {
+                  name: "Price",
+                  value: "$" + price,
+                  inline: false,
+                }
+              )
+              .setThumbnail(img)
+              .setFooter(
+                message.guild.name + " - " + message.author.username,
+                message.guild.iconURL()
+              );
+            message.channel.send(embed);
+          })
+          .catch(() => (error) => {
+            const embederror = new Discord.MessageEmbed()
+              .setColor("#f09719")
+              .setDescription("No product found, try another search.");
+            message.send.channel(embederror);
+            console.log(error);
+          });
+      } catch (error) {
+        const embederror = new Discord.MessageEmbed()
+          .setColor("#f09719")
+          .setDescription("No product found, try another search.");
+        message.send.channel(embederror);
+        console.log(error);
+      }
+    }
+  });
+  client.login(token);
+}
+// client.on("message", (msg) => {
+//   if (msg.content === "ping") {
+//     msg.reply("Pong!");
+//   }
+// });
